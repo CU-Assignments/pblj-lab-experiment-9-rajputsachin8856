589cbb64a971b4e4f55dfc684919d19a28a78d74
rajputsachin8856 <rajputsachin8856@gmail.com>
2025-04-18 11:14:58 +0530

    Create 9.2.dao.java

---
+++ 9.2.dao.java
@@ -0,0 +1,39 @@
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class StudentDao {
+
+    public void createStudent(Student student) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(student);
+        tx.commit();
+        session.close();
+    }
+
+    public Student getStudent(int id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Student student = session.get(Student.class, id);
+        session.close();
+        return student;
+    }
+
+    public void updateStudent(Student student) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.update(student);
+        tx.commit();
+        session.close();
+    }
+
+    public void deleteStudent(int id) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        Student student = session.get(Student.class, id);
+        if (student != null) {
+            session.delete(student);
+        }
+        tx.commit();
+        session.close();
+    }
+}
